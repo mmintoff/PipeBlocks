7b5a85ea744ecd2c9e8f2ce7cfc34843e210107e
Mark Mintoff <mark.mintoff@kingmakers.com>
2025-06-25 17:16:41 +0200

    Namespace flattening tests update

--- src/MM.PipeBlocks.Test/FuncBlockTests.cs
+++ src/MM.PipeBlocks.Test/FuncBlockTests.cs
@@ -17,7 +17,7 @@ public class FuncBlockTests
         // Assert
         Assert.True(actionExecuted);
         Assert.Same(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass0_0.<Execute_WithSimpleAction_ExecutesAction>g__simpleAction|0)", actionBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass0_0.<Execute_WithSimpleAction_ExecutesAction>g__simpleAction|0)", actionBlock.ToString());
     }
 
     [Fact]
@@ -36,7 +36,7 @@ public class FuncBlockTests
         // Assert
         Assert.True(actionExecuted);
         Assert.Same(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass1_0.<Execute_WithValueAction_ExecutesAction>g__valueAction|0)", actionBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass1_0.<Execute_WithValueAction_ExecutesAction>g__valueAction|0)", actionBlock.ToString());
     }
 
     [Fact]
@@ -70,7 +70,7 @@ public class FuncBlockTests
                 Assert.Equal(context.CorrelationId, f.CorrelationId);
                 Assert.Equal("Intentional", f.FailureReason);
                 Assert.True(actionExecuted);
-                Assert.Equal("MM.PipeBlocks.Blocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass2_0.<Execute_WithValueAction_ReturnsFailureContext>g__valueAction|0)", actionBlock.ToString());
+                Assert.Equal("MM.PipeBlocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass2_0.<Execute_WithValueAction_ReturnsFailureContext>g__valueAction|0)", actionBlock.ToString());
             },
             _ => Assert.Fail("Expected a failure"));
     }
@@ -101,7 +101,7 @@ public class FuncBlockTests
                 Assert.Equal(context.CorrelationId, f.CorrelationId);
                 Assert.Equal("Initial Failure", f.FailureReason);
                 Assert.False(actionExecuted);
-                Assert.Equal("MM.PipeBlocks.Blocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass3_0.<Execute_WithFailContext>g__valueAction|0)", actionBlock.ToString());
+                Assert.Equal("MM.PipeBlocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass3_0.<Execute_WithFailContext>g__valueAction|0)", actionBlock.ToString());
             },
             _ => Assert.Fail("Expected a failure"));
     }
@@ -128,7 +128,7 @@ public class FuncBlockTests
         // Assert
         Assert.True(actionExecuted);
         Assert.Same(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass4_0.<Execute_WithSimpleAction_ExecutesAction_Async>g__simpleAction|0)", actionBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass4_0.<Execute_WithSimpleAction_ExecutesAction_Async>g__simpleAction|0)", actionBlock.ToString());
     }
 
     [Fact]
@@ -151,7 +151,7 @@ public class FuncBlockTests
         // Assert
         Assert.True(actionExecuted);
         Assert.Same(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass5_0.<Execute_WithValueAction_ExecutesAction_Async>g__valueAction|0)", actionBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass5_0.<Execute_WithValueAction_ExecutesAction_Async>g__valueAction|0)", actionBlock.ToString());
     }
 
     [Fact]
@@ -186,7 +186,7 @@ public class FuncBlockTests
                 Assert.Equal(context.CorrelationId, f.CorrelationId);
                 Assert.Equal("Intentional", f.FailureReason);
                 Assert.True(actionExecuted);
-                Assert.Equal("MM.PipeBlocks.Blocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass6_0.<Execute_WithValueAction_ReturnsFailureContext_Async>g__valueAction|0)", actionBlock.ToString());
+                Assert.Equal("MM.PipeBlocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass6_0.<Execute_WithValueAction_ReturnsFailureContext_Async>g__valueAction|0)", actionBlock.ToString());
             },
             _ => Assert.Fail("Expected a failure"));
     }
@@ -221,7 +221,7 @@ public class FuncBlockTests
                 Assert.Equal(context.CorrelationId, x.CorrelationId);
                 Assert.Equal("Initial Failure", x.FailureReason);
                 Assert.False(actionExecuted);
-                Assert.Equal("MM.PipeBlocks.Blocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass7_0.<Execute_WithFailContext_Async>g__valueAction|0)", actionBlock.ToString());
+                Assert.Equal("MM.PipeBlocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests+<>c__DisplayClass7_0.<Execute_WithFailContext_Async>g__valueAction|0)", actionBlock.ToString());
             },
             x => Assert.Fail("Expected a failure"));
     }
@@ -240,7 +240,7 @@ public class FuncBlockTests
 
         // Assert
         Assert.NotSame(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsModifiedContext>g__simpleFunc|8_0)", funcBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsModifiedContext>g__simpleFunc|8_0)", funcBlock.ToString());
     }
 
     [Fact]
@@ -272,7 +272,7 @@ public class FuncBlockTests
                 Assert.Equal(initialValue.Identifier, x.Value.Identifier);
                 Assert.Equal(context.CorrelationId, x.CorrelationId);
                 Assert.Equal("Intentional", x.FailureReason);
-                Assert.Equal("MM.PipeBlocks.Blocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsFailureContext>g__simpleFunc|9_0)", funcBlock.ToString());
+                Assert.Equal("MM.PipeBlocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsFailureContext>g__simpleFunc|9_0)", funcBlock.ToString());
             },
             x => Assert.Fail("Expected a failure"));
     }
@@ -291,7 +291,7 @@ public class FuncBlockTests
 
         // Assert
         Assert.NotSame(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsModifiedContext>g__valueFunc|10_0)", funcBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsModifiedContext>g__valueFunc|10_0)", funcBlock.ToString());
     }
 
     [Fact]
@@ -323,7 +323,7 @@ public class FuncBlockTests
                 Assert.Equal(initialValue.Identifier, x.Value.Identifier);
                 Assert.Equal(context.CorrelationId, x.CorrelationId);
                 Assert.Equal("Intentional", x.FailureReason);
-                Assert.Equal("MM.PipeBlocks.Blocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsFailedContext>g__valueFunc|11_0)", funcBlock.ToString());
+                Assert.Equal("MM.PipeBlocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsFailedContext>g__valueFunc|11_0)", funcBlock.ToString());
             },
             x => Assert.Fail("Expected a failure"));
     }
@@ -341,7 +341,7 @@ public class FuncBlockTests
 
         // Assert
         Assert.Same(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsSameContext>g__simpleFunc|12_0)", funcBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsSameContext>g__simpleFunc|12_0)", funcBlock.ToString());
     }
 
     [Fact]
@@ -358,7 +358,7 @@ public class FuncBlockTests
 
         // Assert
         Assert.Same(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsSameContext>g__valueFunc|13_0)", funcBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.FuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsSameContext>g__valueFunc|13_0)", funcBlock.ToString());
     }
 
     /**/
@@ -377,7 +377,7 @@ public class FuncBlockTests
 
         // Assert
         Assert.NotSame(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsModifiedContext_Async>g__simpleFunc|14_0)", funcBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsModifiedContext_Async>g__simpleFunc|14_0)", funcBlock.ToString());
     }
 
     [Fact]
@@ -409,7 +409,7 @@ public class FuncBlockTests
                 Assert.Equal(initialValue.Identifier, x.Value.Identifier);
                 Assert.Equal(context.CorrelationId, x.CorrelationId);
                 Assert.Equal("Intentional", x.FailureReason);
-                Assert.Equal("MM.PipeBlocks.Blocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsFailureContext_Async>g__simpleFunc|15_0)", funcBlock.ToString());
+                Assert.Equal("MM.PipeBlocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsFailureContext_Async>g__simpleFunc|15_0)", funcBlock.ToString());
             },
             x => Assert.Fail("Expected a failure"));
     }
@@ -428,7 +428,7 @@ public class FuncBlockTests
 
         // Assert
         Assert.NotSame(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsModifiedContext_Async>g__valueFunc|16_0)", funcBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsModifiedContext_Async>g__valueFunc|16_0)", funcBlock.ToString());
     }
 
     [Fact]
@@ -460,7 +460,7 @@ public class FuncBlockTests
                 Assert.Equal(initialValue.Identifier, x.Value.Identifier);
                 Assert.Equal(context.CorrelationId, x.CorrelationId);
                 Assert.Equal("Intentional", x.FailureReason);
-                Assert.Equal("MM.PipeBlocks.Blocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsFailedContext_Async>g__valueFunc|17_0)", funcBlock.ToString());
+                Assert.Equal("MM.PipeBlocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsFailedContext_Async>g__valueFunc|17_0)", funcBlock.ToString());
             },
             x => Assert.Fail("Expected a failure"));
     }
@@ -478,7 +478,7 @@ public class FuncBlockTests
 
         // Assert
         Assert.Same(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsSameContext_Async>g__simpleFunc|18_0)", funcBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithSimpleFunction_ReturnsSameContext_Async>g__simpleFunc|18_0)", funcBlock.ToString());
     }
 
     [Fact]
@@ -495,6 +495,6 @@ public class FuncBlockTests
 
         // Assert
         Assert.Same(context, result);
-        Assert.Equal("MM.PipeBlocks.Blocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsSameContext_Async>g__valueFunc|19_0)", funcBlock.ToString());
+        Assert.Equal("MM.PipeBlocks.AsyncFuncBlock`2[MM.PipeBlocks.Test.MyContext,MM.PipeBlocks.Test.MyValue] (Method: MM.PipeBlocks.Test.FuncBlockTests.<Execute_WithValueFunction_ReturnsSameContext_Async>g__valueFunc|19_0)", funcBlock.ToString());
     }
 }
